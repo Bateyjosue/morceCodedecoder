7ec9db0f1675d7888eaf2803bc6b943c1e9e5bd9
Bateyjosue <josuebatey19@gmail.com>
2022-11-03 14:49:23 +0200

    Add decode_char function

--- morce_code_decoder.rb
+++ morce_code_decoder.rb
@@ -1,18 +1,37 @@
+MORCE_CODE = {'A'=> '.-',     'B'=> '-...',   'C'=> '-.-.', 
+        'D'=> '-..',    'E'=> '.',      'F'=> '..-.',
+        'G'=> '--.',    'H'=> '....',   'I'=> '..',
+        'J'=> '.---',   'K'=> '-.-',    'L'=> '.-..',
+        'M'=> '--',     'N'=> '-.',     'O'=> '---',
+        'P': '.--.',   'Q': '--.-',   'R'=> '.-.',
+        'S'=> '...',    'T'=> '-',      'U': '..-',
+        'V'=> '...-',   'W'=> '.--',    'X'=> '-..-',
+        'Y': '-.--',   'Z': '--..',
+        '0'=> '-----',  '1'=> '.----',  '2'=> '..---',
+        '3'=> '...--',  '4'=> '....-',  '5'=> '.....',
+        '6'=> '-....',  '7'=> '--...',  '8'=> '---..',
+        '9'=> '----.' 
+        }
+
 def decode_char(string)
-  puts string
+  MORCE_CODE.each do |code, alpha| 
+    if alpha == string
+      return code
+    end
+  end
 end
 
-def decode_word(string)
-  puts string
-end
+# def decode_word(string)
+#   puts string
+# end
 
-def decode(string)
-  puts string
-end
+# def decode(string)
+#   puts string
+# end
 
 puts decode_char(".-")
-puts decode_word("-- -.--")
-puts decode("-- -.--   -. .- -- .")
+# puts decode_word("-- -.--")
+# puts decode("-- -.--   -. .- -- .")
 
-# Final Test
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+# # Final Test
+# puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
